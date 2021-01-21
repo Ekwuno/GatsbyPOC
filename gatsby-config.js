134c3aea79144b6946d2e61bba0210290625e604
Ekwuno Obinna <obinnacodes@gmail.com>
2021-01-22 00:28:45 +0400

    We have routes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
-
+        {
+            resolve: `gatsby-source-contentful`,
+            options:{
+                spaceId: `a3wufoevrwxz`,
+                accessToken: `E7TtB1we-MN8JLR7YgEM2yaKVJgiCl7tmrDePzSIQnk`
+            }
+        }
+        
     ]
 }
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function About(){
+  return(
+    <div>
+      Hi I am the about page 
+    </div>
+  )
+}
